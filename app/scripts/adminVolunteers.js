2ea160be02016465c1397b2b304f1be0a91dd45e
Mitch Stark <Mitch Stark>
2016-12-15 17:48:52 -0500

    added remove button, needs to be reformatted

--- app/scripts/adminVolunteers.js
+++ app/scripts/adminVolunteers.js
@@ -7,8 +7,16 @@ import Volunteer from './adminVolunteer.js'
 module.exports = React.createClass({
   render: function() {
     var volunteerNodes = this.props.data.map(function(volunteer) {
+      let removeVolunteer = function(e) {
+        console.log(volunteer)
+      }
       return (
-        <Volunteer name={volunteer}/>
+        <div>
+          <Volunteer name={volunteer}/>
+          <form className="removeVolunteer" onSubmit={removeVolunteer}>
+            <input type="submit" value="remove"/>
+          </form>
+        </div>
       );
     });
     return (
